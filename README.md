03c1b46f63fea601f55504e68b9463214f7b75c6
Joel Terrell <falconreid@hotmail.com>
2020-07-25 09:46:25 -0600

    added save/delete functionality to project, styling edits

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # 5-Work-Day-Scheduler
-Create a simple calendar application that allows the user to save events for each hour of the day. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
+
+I created a simple calendar application that allows the user to save events for each hour of the day. This app runs in the browser and features dynamically updated HTML and CSS powered by jQuery and JavaScript.
+
+Task Columns change color based on the time. Present is a linen color, past is grey and future is a light blue.
+
+Save button sends data to localStorage for each corresponding task
+Delete button removes data from localStorage for each corresponding task
+
+work day from 8 - 6 depending on varying schedule
--- scripts/script.js
+++ scripts/script.js
@@ -13,7 +13,6 @@ $(document).ready(function () {
 
   const update = function () {
     presentDay.textContent = presentMoment;
-    console.log(presentMoment);
   };
   setInterval(update, 1000);
 
@@ -26,8 +25,8 @@ $(document).ready(function () {
     $(".description").each(function () {
       // this = arrayBlock and it is parsed into a string
       var hour = parseInt($(this).attr("hour"));
-      console.log("hour:", hour);
-      console.log(hour, presentTime);
+
+      // if statement determines if the corresponding textarea matches the present time and colors accordingly
       if (hour < presentTime) {
         $(this).addClass("past");
       } else if (hour === presentTime) {
@@ -44,21 +43,38 @@ $(document).ready(function () {
   setInterval(update, 1000);
 
   // create click events for save and delete buttons.
-  // save buttons
+  // save buttons target id of given class and saves data from corresponding textarea to localStorage
   function saveButtons() {
-    var svArray = $(".svBtn");
-
-    $(".svBtn").each(function () {
-      var txtAreaArray = $("textarea[hour]").val();
-
-      $(".svBtn").on("click", function (e) {
-        e.preventDefault();
-
-        $(".description[hour]").localStorage.setItem("Task", txtAreaArray);
-        console.log(txtAreaArray);
-      });
+    $(".svBtn").on("click", function (e) {
+      e.preventDefault();
+      var hour = e.target.id.substring(4); //img-18
+      var text = $("#text-" + hour);
+      localStorage.setItem(hour, text.val());
     });
   }
 
+  // delete buttons target id of given class and deletes data from corresponding textarea from localStorage
+  function deleteButtons() {
+    $(".delBtn").on("click", function (e) {
+      e.preventDefault();
+      var hour = e.target.id.substring(7);
+      console.log("Deleteing: " + hour);
+      $("#text-" + hour).val("");
+      localStorage.removeItem(hour);
+    });
+  }
+
+  // for loop takes tasks (as array) from localStorage, converts to string and cycles through each text area with id of text- and places value
+  function loadTasks() {
+    for (i = 8; i <= 18; i++) {
+      var task = localStorage.getItem(i.toString());
+      if (task) {
+        $("#text-" + i).val(task);
+      }
+    }
+  }
+
   saveButtons();
+  deleteButtons();
+  loadTasks();
 });
